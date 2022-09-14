1dcf9f13ef2f37da7924c6f3351846ff2690c642
N8 <n8@n8programs.com>
2022-09-14 09:13:37 -0400

    add modelmatrix support

--- main.js
+++ main.js
@@ -160,14 +160,17 @@ async function main() {
             uniform bool chromaticAberration;
             uniform mat4 projectionMatrixInv;
             uniform mat4 viewMatrixInv;
+            uniform mat4 modelMatrix;
             uniform vec2 resolution;
             uniform bool chromaticAbberation;
             uniform float aberrationStrength;
-            vec3 totalInternalReflection(vec3 ro, vec3 rd, vec3 normal, float ior) {
+            vec3 totalInternalReflection(vec3 ro, vec3 rd, vec3 normal, float ior, mat4 modelMatrixInverse) {
                 vec3 rayOrigin = ro;
                 vec3 rayDirection = rd;
                 rayDirection = refract(rayDirection, normal, 1.0 / ior);
                 rayOrigin = vWorldPosition + rayDirection * 0.001;
+                rayOrigin = (modelMatrixInverse * vec4(rayOrigin, 1.0)).xyz;
+                rayDirection = normalize((modelMatrixInverse * vec4(rayDirection, 0.0)).xyz);
                 for(float i = 0.0; i < bounces; i++) {
                     uvec4 faceIndices = uvec4( 0u );
                     vec3 faceNormal = vec3( 0.0, 0.0, 1.0 );
@@ -185,27 +188,29 @@ async function main() {
                     rayDirection = reflect(rayDirection, faceNormal);
                     rayOrigin = hitPos + rayDirection * 0.01;
                 }
+                rayDirection = normalize((modelMatrix * vec4(rayDirection, 0.0)).xyz);
                 return rayDirection;
             }
             void main() {
+                mat4 modelMatrixInverse = inverse(modelMatrix);
                 vec2 uv = gl_FragCoord.xy / resolution;
                 vec3 directionCamPerfect = (projectionMatrixInv * vec4(uv * 2.0 - 1.0, 0.0, 1.0)).xyz;
                 directionCamPerfect = (viewMatrixInv * vec4(directionCamPerfect, 0.0)).xyz;
                 directionCamPerfect = normalize(directionCamPerfect);
                 vec3 normal = vNormal;
-                vec3 rayOrigin = vec3(cameraPosition);
+                vec3 rayOrigin = cameraPosition;
                 vec3 rayDirection = normalize(vWorldPosition - cameraPosition);
                 vec3 finalColor;
                 if (chromaticAberration) {
-                vec3 rayDirectionR = totalInternalReflection(rayOrigin, rayDirection, normal, max(ior * (1.0 - aberrationStrength), 1.0));
-                vec3 rayDirectionG = totalInternalReflection(rayOrigin, rayDirection, normal, max(ior, 1.0));
-                vec3 rayDirectionB = totalInternalReflection(rayOrigin, rayDirection, normal, max(ior * (1.0 + aberrationStrength), 1.0));
+                vec3 rayDirectionR = totalInternalReflection(rayOrigin, rayDirection, normal, max(ior * (1.0 - aberrationStrength), 1.0), modelMatrixInverse);
+                vec3 rayDirectionG = totalInternalReflection(rayOrigin, rayDirection, normal, max(ior, 1.0), modelMatrixInverse);
+                vec3 rayDirectionB = totalInternalReflection(rayOrigin, rayDirection, normal, max(ior * (1.0 + aberrationStrength), 1.0), modelMatrixInverse);
                 float finalColorR = textureGrad(envMap, rayDirectionR, dFdx(correctMips ? directionCamPerfect: rayDirection), dFdy(correctMips ? directionCamPerfect: rayDirection)).r;
                 float finalColorG = textureGrad(envMap, rayDirectionG, dFdx(correctMips ? directionCamPerfect: rayDirection), dFdy(correctMips ? directionCamPerfect: rayDirection)).g;
                 float finalColorB = textureGrad(envMap, rayDirectionB, dFdx(correctMips ? directionCamPerfect: rayDirection), dFdy(correctMips ? directionCamPerfect: rayDirection)).b;
                 finalColor = vec3(finalColorR, finalColorG, finalColorB) * color;
                 } else {
-                    rayDirection = totalInternalReflection(rayOrigin, rayDirection, normal, max(ior, 1.0));
+                    rayDirection = totalInternalReflection(rayOrigin, rayDirection, normal, max(ior, 1.0), modelMatrixInverse);
                     finalColor = textureGrad(envMap, rayDirection, dFdx(correctMips ? directionCamPerfect: rayDirection), dFdy(correctMips ? directionCamPerfect: rayDirection)).rgb;
                     finalColor *= color;
                 }
@@ -332,6 +337,9 @@ async function main() {
         diamond.material.uniforms.correctMips.value = effectController.correctMips;
         diamond.material.uniforms.chromaticAberration.value = effectController.chromaticAberration;
         diamond.material.uniforms.aberrationStrength.value = effectController.aberrationStrength;
+        diamond.rotation.y += 0.01;
+        diamond.updateMatrix();
+        diamond.updateMatrixWorld();
         renderer.setRenderTarget(defaultTexture);
         renderer.clear();
         renderer.render(scene, camera);
